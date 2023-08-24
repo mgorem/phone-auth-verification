4253b69be3abcbf0435d5712ef597717d3f7a2d9
/omg <oremgitonga22@gmail.com>
2023-08-24 15:49:32 +0300

    add country_picker successful

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:country_picker/country_picker.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -10,10 +9,104 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController phoneController = TextEditingController();
+
+  Country selectedCountry = Country(
+      phoneCode: "254",
+      countryCode: "KE",
+      e164Sc: 0,
+      geographic: true,
+      level: 1,
+      name: "Kenya",
+      example: "Kenya",
+      displayName: "Kenya",
+      displayNameNoCountryCode: "KE",
+      e164Key: "");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(),
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+            child: Column(
+              children: [
+                Container(
+                  width: 200,
+                  height: 200,
+                  padding: const EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.purple.shade50,
+                  ),
+                  child: Image.asset("assets/image2.png"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Register",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Enter your phone number. We'll send you a verification codes.",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  cursorColor: Colors.purple,
+                  controller: phoneController,
+                  decoration: InputDecoration(
+                      hintText: "Enter Phone Number",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.black12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.black12),
+                      ),
+                      prefixIcon: Container(
+                        padding: const EdgeInsets.all(8.0),
+                        child: InkWell(
+                          onTap: () {
+                            showCountryPicker(context: context,
+                                countryListTheme: const CountryListThemeData(
+                                  bottomSheetHeight: 550
+                                ),
+                                onSelect: (value){
+                              setState(() {
+                                selectedCountry = value;
+                              });
+                            });
+                          },
+                          child: Text("${selectedCountry.flagEmoji} + ${selectedCountry.phoneCode}",
+                          style: TextStyle(fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,),),
+                        ),
+                      ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
