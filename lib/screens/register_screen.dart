3b5459892b2b305cf3926d9d9e23f7e43266efe1
/omg <oremgitonga22@gmail.com>
2023-08-25 15:10:46 +0300

    padding on flag, add done icon, add login button

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:country_picker/country_picker.dart';
+import 'package:verifyphone/widgets/custom_button.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -25,6 +26,10 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   @override
   Widget build(BuildContext context) {
+
+    phoneController.selection = TextSelection.fromPosition(
+      TextPosition(offset: phoneController.text.length,),
+    );
     return Scaffold(
       body: SafeArea(
         child: Center(
@@ -56,7 +61,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   height: 10,
                 ),
                 const Text(
-                  "Enter your phone number. We'll send you a verification codes.",
+                  "Enter your phone number. We'll send you a verification code.",
                   style: TextStyle(
                     fontSize: 14,
                     color: Colors.black38,
@@ -70,38 +75,84 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 TextFormField(
                   cursorColor: Colors.purple,
                   controller: phoneController,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      phoneController.text = value;
+                    });
+                  },
                   decoration: InputDecoration(
-                      hintText: "Enter Phone Number",
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: const BorderSide(color: Colors.black12),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: const BorderSide(color: Colors.black12),
-                      ),
-                      prefixIcon: Container(
-                        padding: const EdgeInsets.all(8.0),
-                        child: InkWell(
-                          onTap: () {
-                            showCountryPicker(context: context,
-                                countryListTheme: const CountryListThemeData(
-                                  bottomSheetHeight: 550
-                                ),
-                                onSelect: (value){
-                              setState(() {
-                                selectedCountry = value;
+                    hintText: "Enter Phone Number",
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 15,
+                      color: Colors.grey.shade600,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.black12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.black12),
+                    ),
+                    prefixIcon: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      child: InkWell(
+                        onTap: () {
+                          showCountryPicker(
+                              context: context,
+                              countryListTheme: const CountryListThemeData(
+                                  bottomSheetHeight: 550),
+                              onSelect: (value) {
+                                setState(() {
+                                  selectedCountry = value;
+                                });
                               });
-                            });
-                          },
-                          child: Text("${selectedCountry.flagEmoji} + ${selectedCountry.phoneCode}",
-                          style: TextStyle(fontSize: 18,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,),),
+                        },
+                        child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            "${selectedCountry.flagEmoji} + ${selectedCountry.phoneCode}",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
                       ),
+                    ),
+                    suffixIcon: phoneController.text.length > 9 ? Container(
+                      height: 30,
+                      width: 30,
+                      margin: const EdgeInsets.all(10.0),
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.green
+                      ),
+                      child: const Icon(
+                        Icons.done,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                    )
+                        : null,
                   ),
                 ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: CustomButton(
+                    text: "Login",
+                    onPressed: () {},
+                  ),
+                )
               ],
             ),
           ),
